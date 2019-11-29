c2f6988f14dfbf253b752ff6cca3be8bfa02b5c5
Eswaramoorthi <eswaramoorthi.b@neosofttech.com>
2019-11-29 11:26:16 +0530

    payment done

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,6 +3,6 @@ class Video < ApplicationRecord
 	validates :price, presence: true   
 	validates :description, presence: true 
 
-	has_attached_file :movie, :styles => { :medium => {:geometry => "640*480", :format  => 'mp4'}, :thumb => {:geometry => "640*480", :format => 'jpg', :time => 10}}, :processor => [:transcoder]
+	#has_attached_file :movie, :styles => { :medium => {:geometry => "640*480", :format  => 'mp4'}, :thumb => {:geometry => "640*480", :format => 'jpg', :time => 10}}, :processor => [:transcoder]
 	validates_attachment_content_type :movie, content_type: /\Avideo\/.*\z/
 end
